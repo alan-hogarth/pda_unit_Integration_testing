346295c5e97829348bd9d81fad3978049182aa9b
Alan Hogarth <alan.hogarth@outlook.com>
2021-01-24 13:01:33 +0000

    4 integration tests passing

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -38,5 +38,23 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('12')
   })
 
+  it('can perform multiple operations with number types', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number1')).click();
+    element(by.css('#number8')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#number9')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('0.487')
+  })
 
 });
